481ea722dfd129a079c460e78af2099b736b3b2d
Antsuhue <anderson_julio_15@hotmail.com>
2021-04-23 14:56:28 -0300

    Decodificaçao do token

--- src/controller/services/bankController.js
+++ src/controller/services/bankController.js
@@ -1,11 +1,16 @@
+const user = require("../../model/user")
+const jwt = require('jsonwebtoken')
+
 function depositarDinheiro (req,res){
     const body = req.body
     const valor_deposito = body.valor
 
-    const t = req.headers.token
+    const user_token = req.headers.authorization
+    const token = user_token.split(' ')
+    const decode = jwt.verify(token[0], process.env.SECRET)
 
-    
-    
+    console.log(decode.chave);
+
 
     return res.status(200).json({status:"valor depositado"})
 }
